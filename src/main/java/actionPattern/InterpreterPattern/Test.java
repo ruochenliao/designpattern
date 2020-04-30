5964a1273dca55f61b42aaa11e5ecf83f7b8f695
john <lrc272317227@gmail.com>
2020-04-30 20:59:48 +0800

    解释器

---
+++ src/main/java/actionPattern/InterpreterPattern/Test.java
@@ -0,0 +1,14 @@
+package actionPattern.InterpreterPattern;
+
+/**
+ * 解释器模式
+ *
+ * 应用
+ * 解释语言文字，数字等，让计算器，计算机能够理解
+ *
+ * 定义
+ * 所谓解释器模式就是定义"语言的文法"，并建立一个解释器来解释语言中的句子，比如数学，正则表达式
+ */
+public class Test {
+
+}
